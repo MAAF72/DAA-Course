1cce1824cac491a13fdfa39bbe3684750fc8bf8d
Al Fatih <abdurrahmanalfatih72@gmail.com>
2020-05-01 11:04:24 +0700

    Adjust chart height and fix duplicate data
    Adjust chart height and fix duplicate data

--- Tupro/js/compare.js
+++ Tupro/js/compare.js
@@ -9,7 +9,22 @@ $(function() {
     var worker = new Worker('js/worker.js');
     
     worker.addEventListener('message', (e) => {
-        data.push(e.data);
+        var duplicate = false;
+        for (var i = 0; i < data.length; i++) {
+            if (data[i].num == e.data.num) {
+                data[i].merge = ((data[i].merge * data[i].count) + e.data.merge) / (data[i].count + 1);
+                data[i].bubble = ((data[i].bubble * data[i].count) + e.data.bubble) / (data[i].count + 1);
+                data[i].count++;
+                duplicate = true;
+                break;
+            }
+        }
+        
+        if (!duplicate) {
+            data.push(e.data);
+            data[data.length - 1].count = 1;
+        }
+        
         if (!compareBar) {
            compareBar = Morris.Bar({
                 element: 'bar-chart',
@@ -20,6 +35,7 @@ $(function() {
                 hideHover: 'auto',
                 hoverCallback: (index, options, content, row) => {
                     return `
+                    <div>The number of trials is ${row.count}</div>
                     <div>The number of element is ${row.num}</div>
                     <div style="color: rgb(11, 98, 164);">Merge Sort takes ${row.merge} ms</div>
                     <div style="color: rgb(122, 146, 163);">Bubble Sort takes ${row.bubble} ms</div>
